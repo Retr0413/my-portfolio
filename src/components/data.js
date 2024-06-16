765b16362ffe96730d1c34ce6b793cfca46136f5
Retr0413 <153156670+Retr0413@users.noreply.github.com>
2024-06-17 00:33:02 +0900

    3

--- src/components/data.js
+++ src/components/data.js
@@ -4,27 +4,27 @@ export const projectData = [
         title: 'リズムゲーム',
         description: 'Unityを使って作成したゲームになります。アルバイトの卒業生に向けたゲームです。',
         language: '使用言語:C#',
-        backgroundImage: ''
+        backgroundImage: '/images/Mark.PNG'
     },
     {
         id: 2,
         title: 'ダイクストラ法アルゴリズム',
         description: 'C言語を使用して作成したダイクストラ法のアルゴリズムです。',
         language: '使用言語:C',
-        backgroundImage: ''
+        backgroundImage: '/images/Mark.PNG'
     },
     {
         id: 3,
         title: 'Todoリストアプリ',
         description: 'バイト先でのやることをまとめやすくしたWebアプリです。フレームワークはDjangoを使用',
         language: '使用言語:Python',
-        backgroundImage: ''
+        backgroundImage: '/images/Mark.PNG'
     },
     {
         id: 4,
         title: '簡単なRSA暗号',
         description: 'Pythonを使用して作成した簡単なRSA暗号です。情報セキュリティに興味があり、教材を基に作成しました。',
         language: '使用言語:Python　参考文献:Pythonでいかにして暗号を破るか',
-        backgroundImage: ''
+        backgroundImage: '/images/Mark.PNG'
     },
 ];
